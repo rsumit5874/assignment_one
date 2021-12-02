b4946f753224de69a0ee609ec28952101cd61d26
Sumit Rai <rsumit.neosoft@gmail.com>
2021-12-02 13:10:45 +0530

    intial commit

---
+++ android/app/src/main/kotlin/com/neosoft/assignment_one/assignment_one/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.neosoft.assignment_one.assignment_one
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: () {
+        return const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          themeMode: ThemeMode.system,
+        );
+      },
+    );
+  }
+}
